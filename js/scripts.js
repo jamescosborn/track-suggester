b6abf4a24245e684d8865fa25e5d6b0504194dbc
James Osborn <jamescarlosborn@gmail.com>
2017-09-15 11:00:05 -0700

    add javascript, jquery, if/else statements, establish basic functionality, replace radio buttons with select boxes.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form#trackSuggester").submit(function(event) {
+    var whichTeam = $("#whichTeam").val();
+    var whichLang = $("#whichLang").val();
+    var whichCpu = $("#whichCpu").val();
+
+    if (whichLang === "1" && whichCpu === "1") {
+      var thisTrack = "C#/.Net"
+    }
+    else if (whichLang === "2" && whichCpu === "2") {
+      var thisTrack = "Ruby/Rails"
+    }
+    else if (whichTeam === "2" && whichLang === "2") {
+      var thisTrack = "PHP/Drupal"
+    }
+    $("#track").append(thisTrack);
+    $("#trackSuggestion").show();
+
+  event.preventDefault();
+ });
+});
